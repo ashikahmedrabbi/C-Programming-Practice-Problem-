f8138d89ee6056e70b179c49f771f912ce4c38f1
unknown <ashikahmedrabbi@gmail.com>
2023-04-04 00:17:07 +0600

    Day 1

---
+++ practiceProblem3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int b;
+    scanf("%d ", &b);
+    if (b % 2 == 0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+
+    return 0;
+}
